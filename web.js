553b9e5299ad1299d9f6a6ca9d0855b103d7a01a
Rajesh <rajesh@walmartlabs.com>
2013-07-05 14:53:04 -0700

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile('./index.html', function (err, data) {
+	  if (err) throw err;
+		resBuf = Buffer(data);
+		response.send(resBuf.toString());
+	});
+
 });
 
 var port = process.env.PORT || 5000;
